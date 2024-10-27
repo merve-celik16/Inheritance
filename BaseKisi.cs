190fa0c51e54b1feac857a783a9d274331e5207a
merve-celik16 <mervedursunn34@gmail.com>
2024-10-28 00:32:07 +0300

    Proje dosyası ekle.

---
+++ BaseKisi.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    public class BaseKisi
+    {
+        public string Ad { get; set; }
+        public string Soyadi { get; set; }
+        public void BaseKisiBilgi()
+        {
+            Console.WriteLine($"Adı :{Ad}  \t Soyadı : {Soyadi}");
+        }
+    }
+
+    public class Ogrenci : BaseKisi
+    {
+        public int OgrenciNumarasi { get; set; }
+        public void OgrenciBilgi()
+        {
+            Console.WriteLine($" Adı : {Ad} \t Soyadi : {Soyadi} \t Numarası : {OgrenciNumarasi}");
+        }
+    }
+
+
+
+    public class Ogretmen : BaseKisi
+    {
+        public decimal Maas { get; set; }
+        public void OgretmenBilgisi()
+        {
+            Console.WriteLine($" Adı : {Ad} \t Soyadi : {Soyadi}  \t Maaşı : {Maas}");
+        }
+    }
+
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Inheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Öğrenci nesnesi oluşturma ve değer atama
+            Ogrenci ogrenci1 = new Ogrenci();
+            ogrenci1.Ad = "Merve";
+            ogrenci1.Soyadi = "Çelik";
+            ogrenci1.OgrenciNumarasi = 12345;
+
+            // Öğretmen nesnesi oluşturma ve değer atama
+            Ogretmen ogretmen1 = new Ogretmen();
+            ogretmen1.Ad = "Kaan";
+            ogretmen1.Soyadi = "Tunç";
+            ogretmen1.Maas = 15000;
+
+            // Bilgileri yazdırma
+            ogrenci1.OgrenciBilgi();
+            Console.WriteLine();
+            ogretmen1.OgretmenBilgisi();
+        }
+    }
+}
